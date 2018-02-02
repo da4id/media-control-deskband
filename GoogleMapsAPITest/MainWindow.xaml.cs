58368e994c5b313e0ef7cf0670412e26e372b249
da4id <mail@davidzingg.ch>
2018-02-02 16:26:17 +0100

    Added Google maps API to hello world bar

---
+++ GoogleMapsAPITest/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Maps.Common.Enums;
+using GoogleApi.Entities.Maps.DistanceMatrix.Request;
+using GoogleApi.Entities.Maps.DistanceMatrix.Response;
+using Newtonsoft.Json;
+
+namespace GoogleMapsAPITest
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            googleRequest();
+        }
+
+        public void googleRequest()
+        {
+            var req = new DistanceMatrixRequest();
+            req.Destinations = new List<Location>{ new Location("4052 Basel")};
+            req.Origins = new List<Location>{ new Location("Kägenstrasse 4153 Reinach")};
+            req.DepartureTime = DateTime.Now;
+            req.TravelMode = TravelMode.Driving;
+            req.Key = "API Key";
+            req.TrafficModel = TrafficModel.Pessimistic;
+            
+            var resp = GoogleApi.GoogleMaps.DistanceMatrix.Query(req);
+
+            var parsedResponse = JsonConvert.DeserializeObject<DistanceMatrixParser.myDistanceMatrixResponse>(resp.RawJson);
+
+            lbDuration.Content = parsedResponse.Rows.First().Elements.First().DurationInTraffic.Text;
+        }
+    }
+}
--- SampleBars/HelloWorldBar.cs
+++ SampleBars/HelloWorldBar.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.ComponentModel;
 using System.Windows.Forms;
-
 using BandObjectLib;
 using System.Runtime.InteropServices;
-using WindowsInput.Native;
-using WindowsInput;
 using System.Drawing;
+using System.Linq;
+using System.Collections.Generic;
+
+using GoogleApi.Entities.Common;
+using GoogleApi.Entities.Common.Enums;
+using GoogleApi.Entities.Maps.Common.Enums;
+using GoogleApi.Entities.Maps.DistanceMatrix.Request;
+using GoogleApi.Entities.Maps.DistanceMatrix.Response;
+using Newtonsoft.Json;
 
 namespace SampleBars
 {
@@ -14,15 +20,22 @@ namespace SampleBars
     [BandObject("Hello World Bar", BandObjectStyle.Horizontal | BandObjectStyle.ExplorerToolbar | BandObjectStyle.TaskbarToolBar, HelpText = "Shows bar that says hello.")]
     public class HelloWorldBar : BandObject
     {
-        private System.Windows.Forms.Button button1;
-        private TableLayoutPanel tableLayoutPanel1;
-        private Button button2;
-        private Button button3;
         private System.ComponentModel.Container components = null;
+        private Timer tmr1 = new Timer();
 
         public HelloWorldBar()
         {
             InitializeComponent();
+            tmr1.Interval = 15*1000;
+            tmr1.Tick += tmr1_Tick;
+            tmr1.Start();
+            requestTrafficFromGoogle();
+        }
+
+        void tmr1_Tick(object sender, EventArgs e)
+        {
+            tmr1.Start();
+            requestTrafficFromGoogle();
         }
 
         protected override void Dispose(bool disposing)
@@ -35,117 +48,52 @@ namespace SampleBars
             base.Dispose(disposing);
         }
 
+        public void requestTrafficFromGoogle()
+        {
+            try
+            {
+                var req = new DistanceMatrixRequest();
+                req.Destinations = new List<Location> { new Location("4052 Basel") };
+                req.Origins = new List<Location> { new Location(Properties.Settings.Default.Abfahrtsort) };
+                req.TravelMode = TravelMode.Driving;
+                req.DepartureTime = DateTime.Now;
+                req.Key = "API Key";
+                req.TrafficModel = TrafficModel.Pessimistic;
+                var test = req.GetQueryStringParameters();
+
+                var resp = GoogleApi.GoogleMaps.DistanceMatrix.Query(req);
+
+                var parsedResponse = JsonConvert.DeserializeObject<DistanceMatrixParser.myDistanceMatrixResponse>(resp.RawJson);
+
+                if (resp.Status == GoogleApi.Entities.Common.Enums.Status.Ok)
+                {
+                    if(parsedResponse.Rows.First().Elements.First().DurationInTraffic != null)
+                    {
+                        this.Title = parsedResponse.Rows.First().Elements.First().DurationInTraffic.Text;
+                    }
+                }
+            }
+            catch
+            {
+                this.Title = "Fehler";
+            }
+        }
+
         #region Component Designer generated code
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.Black;
-            this.button1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button1.Location = new System.Drawing.Point(0, 0);
-            this.button1.Margin = new System.Windows.Forms.Padding(0);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(30, 30);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "⏮";
-            this.button1.UseVisualStyleBackColor = false;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 3;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
-            this.tableLayoutPanel1.Controls.Add(this.button3, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.button2, 2, 0);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 1;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(92, 30);
-            this.tableLayoutPanel1.TabIndex = 1;
-            // 
-            // button3
-            // 
-            this.button3.BackColor = System.Drawing.Color.Black;
-            this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Arial", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button3.Location = new System.Drawing.Point(30, 0);
-            this.button3.Margin = new System.Windows.Forms.Padding(0);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(30, 30);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "▶";
-            this.button3.UseVisualStyleBackColor = false;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
-            // 
-            // button2
-            // 
-            this.button2.BackColor = System.Drawing.Color.Black;
-            this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Segoe UI", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.HighlightText;
-            this.button2.Location = new System.Drawing.Point(60, 0);
-            this.button2.Margin = new System.Windows.Forms.Padding(0);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(32, 30);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "⏭";
-            this.button2.UseVisualStyleBackColor = false;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // HelloWorldBar
             // 
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.MinSize = new System.Drawing.Size(92, 30);
+            this.MinSize = new System.Drawing.Size(0, 30);
             this.Name = "HelloWorldBar";
-            this.Size = new System.Drawing.Size(92, 30);
+            this.Size = new System.Drawing.Size(0, 30);
             this.Title = "Hello Bar";
-            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
         #endregion
-
-        private void button1_Click(object sender, System.EventArgs e)
-        {
-            InputSimulator s = new InputSimulator();
-            //s.Keyboard.ModifiedKeyStroke(new [] { VirtualKeyCode.LWIN, VirtualKeyCode.LCONTROL }, VirtualKeyCode.LEFT);
-            s.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PREV_TRACK);
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            InputSimulator s = new InputSimulator();
-            //s.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.LWIN, VirtualKeyCode.LCONTROL }, VirtualKeyCode.RIGHT);
-            s.Keyboard.KeyPress(VirtualKeyCode.MEDIA_NEXT_TRACK);
-        }
-
-        private void button3_Click(object sender, EventArgs e)
-        {
-            InputSimulator s = new InputSimulator();
-            //s.Keyboard.ModifiedKeyStroke(new[] { VirtualKeyCode.LWIN, VirtualKeyCode.LCONTROL }, VirtualKeyCode.RIGHT);
-            s.Keyboard.KeyPress(VirtualKeyCode.MEDIA_PLAY_PAUSE);
-        }
 
     }
 }
